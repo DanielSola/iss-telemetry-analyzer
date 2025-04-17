17e339a9de64c138dadeb3d23d470ac53b443bfa
Daniel Solá <daniel.sola@aizon.ai>
2025-04-17 21:41:44 +0200

    fix NaN change rate

--- src/utils/getChangeRate.go
+++ src/utils/getChangeRate.go
@@ -13,6 +13,12 @@ func GetChangeRate(currentValue float64, previousValue float64, currentTimestamp
 
 	if err != nil {
 		fmt.Printf("Error calculating time diff between: %s and %s: %v\n", currentTimestamp, previousTimestamp, err)
+		return 0 // Return 0 if there's an error in time difference calculation
+	}
+
+	if timeDiff == 0 {
+		fmt.Printf("Time difference is zero between: %s and %s\n", currentTimestamp, previousTimestamp)
+		return 0 // Avoid division by zero
 	}
 
 	return valueDiff / timeDiff
